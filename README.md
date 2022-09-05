d5e469d12067802ed2f34f6b9b96692b225b5d10
brittneypostma <bdesignedwebdev@gmail.com>
2022-09-04 21:17:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-g6b6dq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-g6b6dq)
---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>Made with 💖 by the
+	<a href="https://sveltesirens.dev">Svelte Sirens</a></footer>
+<style>
+	footer {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		text-align: center;
+		padding-bottom: var(--size-2);
+		color: var(--white);
+		font-weight: var(--font-weight-9);
+	}
+</style>
